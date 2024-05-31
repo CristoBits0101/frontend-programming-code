fd600c51c334ea62686de987b0e5d5c3be3fedea
CRPS <cristobits0101@gmail.com>
2024-05-31 11:54:35 +0100

    Adding useState

--- React/Practices/00-first-react-app/src/App.jsx
+++ React/Practices/00-first-react-app/src/App.jsx
@@ -1,13 +1,22 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react"
+import "./App.css"
 
-const useCounter = () => {
+export default function App() {
+  // useState.
+  const [value, setValue] = useState(0)
+
+  // useEffect.
+
   return (
     <div>
-      <h1>Contador: {count}</h1>
-      <button onClick={increment}>Incrementar</button>
+      <h1>The value is: {value}</h1>
+      <button
+        onClick={() => {
+          setValue(value + 1)
+        }}
+      >
+        Update value
+      </button>
     </div>
-  );
-};
-
-export default App;
+  )
+}
