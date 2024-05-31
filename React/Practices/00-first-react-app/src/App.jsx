0e32a126eba7545e7eb40d597a1e2e903e3b74f2
CRPS <cristobits0101@gmail.com>
2024-05-31 11:41:51 +0100

    Add git ignore

--- React/Practices/00-first-react-app/src/App.jsx
+++ React/Practices/00-first-react-app/src/App.jsx
@@ -2,27 +2,12 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 const useCounter = () => {
-  const [count, setCount] = useState(0);
-
-  return {
-    count,
-    increment: () => setCount((count) => count + 1),
-  };
-};
-
-function App() {
-  const { count, increment } = useCounter();
-
-  useEffect(() => {
-    alert("Efecto ejecutado cuando cambia el estado de count al incrementar");
-  }, [count]);
-
   return (
     <div>
       <h1>Contador: {count}</h1>
       <button onClick={increment}>Incrementar</button>
     </div>
   );
-}
+};
 
 export default App;
