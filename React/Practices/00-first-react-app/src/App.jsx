60bc9f64208e3f9098b7d4f845305887eb849845
CRPS <cristobits0101@gmail.com>
2024-06-01 11:55:39 +0100

    Actualizando directorios

--- React/Practices/00-first-react-app/src/App.jsx
+++ React/Practices/00-first-react-app/src/App.jsx
@@ -2,21 +2,33 @@ import React, { useState, useEffect } from "react"
 import "./App.css"
 
 export default function App() {
-  // useState.
-  const [value, setValue] = useState(0)
-
-  // useEffect.
-
+  const [show, setShow] = useState(true)
   return (
-    <div>
-      <h1>The value is: {value}</h1>
-      <button
-        onClick={() => {
-          setValue(value + 1)
-        }}
-      >
-        Update value
+    <>
+      <button onClick={() => {setShow(false)}}>
+        Stop showing
       </button>
-    </div>
+      {show ? <MouseColor /> : null}
+    </>
   )
+}
+
+function MouseColor() {
+  const [color, setColor] = useState("Yellow")
+  useEffect(()=>{
+    function onMouseMove(event) {
+      if (event.clientX < window.innerWidth / 2)
+        setColor("Yellow")
+      else
+        setColor("blue")
+    }
+    window.addEventListener('mousemove', onMouseMove)
+    console.log('Running')
+    return () => {
+      window.removeEventListener('mousemove', onMouseMove)
+      console.log('Cleaning')
+    }
+  }, [])          
+  // console.log("📣 Rendering")
+  return <div style={{height: "100vh", background: color}}></div>
 }
